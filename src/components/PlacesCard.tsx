de0469dc6c2f72af3379824569b1b06f79e6e6fd
Lili00ani <saputri.liliani@gmail.com>
2024-05-11 00:58:23 +0800

    revised places page

---
+++ src/components/PlacesCard.tsx
@@ -0,0 +1,11 @@
+import { Card } from "flowbite-react";
+
+export function PlacesCard() {
+  return (
+    <Card href="#" className="max-w-sm">
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        Places name
+      </h5>
+    </Card>
+  );
+}
--- src/pages/placesPage.tsx
+++ src/pages/placesPage.tsx
@@ -1,8 +1,10 @@
+import MapComponent from "../components/Map";
+
 export default function PlacesPage() {
   return (
     <div className="flex">
       <h1>placesPage</h1>
-      <h1>YOYO</h1>
+      <MapComponent />
     </div>
   );
 }
